90448b9b1793f3ab334294a2cbd3d6a216d37a10
danar1 <61307472+danar1@users.noreply.github.com>
2020-10-25 08:17:45 +0200

    Add ips info in output

--- Homework2/output.tf
+++ Homework2/output.tf
@@ -1,3 +1,15 @@
 output "application_load_balancer_dns" {
     value = module.alb.this_lb_dns_name
 }
+
+output "web_servers_public_ip" {
+    value = module.web.public_ip
+}
+
+output "web_servers_private_ip" {
+    value = module.web.private_ip
+}
+
+output "db_servers_private_ip" {
+    value = module.db.private_ip
+}
